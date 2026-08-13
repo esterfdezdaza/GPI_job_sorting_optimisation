d87588f77f150ed6db1ba0a6495f1a7e8eca589f
Fernandez Daza Rodrigo <Ester.Fernandezdazarodrigo@graphicpkg.com>
2026-08-13 12:28:33 +0100

    Adapted for several machines under the same die - working to just have one record per die per machine if exists

--- functions.py
+++ functions.py
@@ -80,5 +80,82 @@ def clean_value(value):
     except ValueError:
         return value
 
+def get_data_from_csv(df):
+    # Dictionary to store all data grouped by die
+    dies = {}
+
+    # First die in the file
+    current_die = clean_value(df.iloc[0, 0])
+
+    current_machine = df.iloc[0, 1]
+
+    # Create an empty list for that die
+    dies[current_die] = []
+
+    # Loop through every row in the dataframe
+    for row in range(len(df)):
+        # Save the first die
+        die = clean_value(df.iloc[row, 0])
+        current_machine = df.iloc[row-1, 1]
+
+        # If we are still talking about the same die
+        if die == current_die:
+
+            # List to store all the average data
+            avrg_data = []
+            
+            for j in range(9):
+                # Store the value we are in the row 
+                value = clean_value(df.iloc[row, j])
+                
+                # If that value is the die number and the following value is None
+                if (value == current_die) & (clean_value(df.iloc[row, 2]) == None):
+                    # We save the machine those values are for
+                    avrg_data.append(current_machine)
+
+                    # Then we iterate in that line to save those values
+                    for i in range(9):
+                        # I get the first value & clean it
+                        value = df.iloc[row, i+2]
+                        value = clean_value(value)
+                        # If the value is not None then I save it
+                        if value != None:
+                            avrg_data.append(value)
+            # Just creating the list if there is a value to avoid empty lists
+            if avrg_data:
+                dies[current_die].append(avrg_data)
+        
+
+        else:
+            current_die = die
+
+            dies[current_die] = []
+
+            avrg_data = []
+
+            for j in range(9):
+                # Store the value we are in the row 
+                value = clean_value(df.iloc[row, j])
+
+                # If that value is the die number and the following value is None
+                if (value == current_die) & (clean_value(df.iloc[row, 2]) == None):
+                    # We save the machine those values are for
+                    avrg_data.append(current_machine)
+                    # Then we iterate in that line to save those values
+                    for i in range(9):
+                        # I get the first value & clean it
+                        value = df.iloc[row, i+2]
+                        value = clean_value(value)
+                        # If the value is not None then I save it
+                        
+                        if value != None:
+                            avrg_data.append(value)
+
+            # Just creating the list if there is a value to avoid empty lists
+            if avrg_data:
+                dies[current_die].append(avrg_data)
+
+
+    return dies
 
 
